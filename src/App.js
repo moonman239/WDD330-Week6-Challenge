3d315fd189251d2a08619b4f45d1b827e068b87f
Montana Burr <bur18019@byui.edu>
2021-05-25 13:28:42 -0600

    Uses objects to represent tests.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ class App extends React.Component
   {
     super();
     this.state = {
-      tasks: []
+      tasks: [],
+      criteria: "All"
     }
   }
   addTask = () => {this.setState({
-    tasks: this.state.tasks.concat([document.getElementById("taskTextbox").value])
+    tasks: this.state.tasks.concat([
+      {name: document.getElementById("taskTextbox").value,
+      completed: false
+      }]),
+    criteria: this.state.criteria
   });
   document.getElementById("taskTextbox").value = "";
 }
 removeTask = (event) => {
   console.log(event.target);
   this.setState({
-  tasks:this.state.tasks.filter((x,index) => index !== parseInt(event.target.id))
+  tasks:this.state.tasks.filter((x,index) => index !== parseInt(event.target.id)),
+  criteria:this.state.criteria
 });
 }
 showTasks(taskList)
@@ -28,12 +34,13 @@ showTasks(taskList)
     let children = [];
     for (const i in taskList)
     {
-        let child = <tr><td><TaskView index={i} onRemoveButtonClick={this.removeTask} name={taskList[i]}></TaskView></td></tr>;
+        let child = <tr><td><TaskView index={i} onRemoveButtonClick={this.removeTask} name={taskList[i].name}></TaskView></td></tr>;
         children.push(child);
     }
     let table = <table>
         <tbody>
             {children}
+            <tr><td></td></tr>
         </tbody>
     </table>;
     return table;
