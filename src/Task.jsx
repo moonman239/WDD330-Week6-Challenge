010eb2d245a8a9eaaff3788bbad5ddabee5ae44d
Montana Burr <bur18019@byui.edu>
2021-05-25 12:07:02 -0600

    Added code to deal with task completion.

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 export class TaskView extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.state = {
+            completed: false
+        };
+    }
+    completedClicked = () => this.setState(
+        {
+            completed: !(this.state.completed)
+        }
+    );
     render()
     {
         const jsx = <div>
-            <input type="checkbox"></input>
+            <input type="checkbox" onClick={this.completedClicked}></input>
+            {this.state.completed ? "<u>" : ""}
             Task
+            {this.state.completed ? "</u>" : ""}
             <button>X</button>
         </div>;
         return jsx;
